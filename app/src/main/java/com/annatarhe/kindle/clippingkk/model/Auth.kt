e7e68bed7a776a7cf26d7a755c3075f2998e4d92
AnnatarHe <iamhele1994@gmail.com>
2018-10-28 00:29:45 +0800

    feat(auth): add auth module and update icon

--- app/src/main/java/com/annatarhe/kindle/clippingkk/model/Auth.kt
+++ app/src/main/java/com/annatarhe/kindle/clippingkk/model/Auth.kt
@@ -1,34 +1,41 @@
 package com.annatarhe.kindle.clippingkk.model
 
-import android.app.DownloadManager
 import android.util.Log
-import com.github.kittinunf.fuel.android.core.Json
 import com.github.kittinunf.fuel.android.extension.responseJson
-import com.github.kittinunf.fuel.core.FuelError
-import com.github.kittinunf.fuel.core.Request
-import com.github.kittinunf.fuel.core.Response
-import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
-import com.github.kittinunf.result.Result
 import com.github.salomonbrys.kotson.jsonObject
 
 class AuthAPI {
 
     data class User(
+        val id: Int,
         val email: String,
-        val avatar: String
+        val avatar: String,
+        val token: String
     )
 
-    fun Auth(email: String, pwd: String) {
+    fun Auth(email: String, pwd: String, onSuccess: (User) -> Nothing?) {
         val params = jsonObject(
             "email" to email,
             "pwd" to pwd
         )
-        "https://api.clippingkk.annatarhe.com/api/auth".httpPost()
+        "https://api.clippingkk.annatarhe.com/api/auth/login".httpPost()
             .jsonBody(params.toString())
             .responseJson { request, response, result ->
                 val resultData = result.get().obj()
                 Log.i("auth", resultData.toString())
+                if (resultData.getInt("status") != 200) {
+                    Log.i("auth", resultData.toString())
+                }
+                val data = resultData.getJSONObject("data")
+                val profile = data.getJSONObject("profile")
+                val user = User(
+                    profile.getInt("id"),
+                    profile.getString("email"),
+                    profile.getString("avatar"),
+                    data.getString("token")
+                )
+                onSuccess(user)
             }
     }
 }
--- app/src/main/java/com/annatarhe/kindle/clippingkk/ui/AuthPage.kt
+++ app/src/main/java/com/annatarhe/kindle/clippingkk/ui/AuthPage.kt
@@ -1,15 +1,13 @@
 package com.annatarhe.kindle.clippingkk.ui
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
 import android.view.View
-import android.widget.EditText
 import com.annatarhe.kindle.clippingkk.R
 import com.annatarhe.kindle.clippingkk.model.AuthAPI
 import kotlinx.android.synthetic.main.activity_auth.*
+import java.security.KeyStore
 
 class AuthPage: AppCompatActivity() {
 
@@ -25,6 +23,16 @@ class AuthPage: AppCompatActivity() {
         Log.i("auth", pwd)
 
         // TODO: handle response
-        AuthAPI().Auth(email, pwd)
+        AuthAPI().Auth(email, pwd, { user ->
+            Log.i("auth", user.avatar)
+            saveToKeyStore(user)
+            return@Auth null
+        })
+    }
+
+    private fun saveToKeyStore(user: AuthAPI.User) {
+        val keystore = KeyStore.getInstance("AndroidKeyStore")
+        // TODO: save to keystore
+        // https://developer.android.com/training/articles/keystore
     }
 }
